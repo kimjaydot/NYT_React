453984b7daafaf728c0a22df11b59e9a611b666b
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-07-23 14:37:12 -0500

    added search

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -1,27 +1,114 @@
 import React from "react";
 
 class Search extends React.Component {
-
   constructor(props) {
     super(props);
+
+    this.state = {
+      term: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    var newState = {};
+    newState[event.target.id] = event.target.value;
+    this.setState(newState);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("CLICK");
+    console.log(this.state.term);
+    this.props.setTerm(this.state.term);
+    this.setState({ term: "" });
   }
 
   render() {
+
     return (
+
       <div className="panel panel-default">
         <div className="panel-heading">
           <h3 className="panel-title text-center">Search</h3>
         </div>
         <div className="panel-body text-center">
 
-          <h1>Address:</h1>
-          <p>{this.props.address}</p>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <h4 className="">
+                <strong>Topic</strong>
+              </h4>
 
+              {/*
+                Note how each of the form elements has an id that matches the state.
+                This is not necessary but it is convenient.
+                Also note how each has an onChange event associated with our handleChange event.
+              */}
+              <input
+                type="text"
+                className="form-control text-center"
+                id="term"
+                value={this.state.term}
+                onChange={this.handleChange}
+                required
+              />
+              <br />
+
+
+    <h4 className="">
+                <strong>Start Year</strong>
+              </h4>
+
+              {/*
+                Note how each of the form elements has an id that matches the state.
+                This is not necessary but it is convenient.
+                Also note how each has an onChange event associated with our handleChange event.
+              */}
+              <input
+                type="text"
+                className="form-control text-center"
+                id="term"
+                value={this.state.term}
+                onChange={this.handleChange}
+                required
+              />
+              <br />
+
+                  <h4 className="">
+                <strong>End Year</strong>
+              </h4>
+
+              {/*
+                Note how each of the form elements has an id that matches the state.
+                This is not necessary but it is convenient.
+                Also note how each has an onChange event associated with our handleChange event.
+              */}
+              <input
+                type="text"
+                className="form-control text-center"
+                id="term"
+                value={this.state.term}
+                onChange={this.handleChange}
+                required
+              />
+              <br />
+
+              <button
+                type="submit"
+                className="btn btn-primary"
+              >
+                Search
+              </button>
+
+            </div>
+          </form>
         </div>
       </div>
     );
   }
 }
 
-// Export the component back for use in other files
 export default Search;
